5c4ecc924519e4983b71437229cceac953ae6fb5
osakana0630 <osakana0630@gmail.com>
2024-04-29 09:05:43 +0900

    fix inquiry page

--- app/inquiry/page.tsx
+++ app/inquiry/page.tsx
@@ -1,63 +1,77 @@
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import BasicLayout from "@/components/layout/basic-layout";
 
 export default async function Page() {
   return (
     <BasicLayout pageTitle="お問い合わせ">
-      <div className="container">
-        <h2 className="font-bold text-3xl mb-6 text-center">
-          お問い合わせフォーム
-        </h2>
+      <div className="container max-w-2xl">
         <form
           method="post"
           action="https://imai.form.newt.so/v1/b1WwN1BIan"
           className="flex flex-col gap-4"
         >
-          <label htmlFor="fullName">
-            氏名
+          <div>
+            <Label htmlFor="fullName">氏名*</Label>
             <Input
               id="fullName"
               type="text"
               name="fullName"
               placeholder="山田太郎"
+              required
             />
-          </label>
-          <label htmlFor="companyName">
-            会社名
+          </div>
+
+          <div>
+            <Label htmlFor="companyName">会社名*</Label>
             <Input
               id="companyName"
               type="text"
               name="companyName"
               placeholder="株式会社〇〇"
+              required
             />
-          </label>
-          <label htmlFor="department">
-            部署
+          </div>
+
+          <div>
+            <Label htmlFor="department">部署</Label>
             <Input
               id="department"
               type="text"
               name="department"
               placeholder="営業部"
             />
-          </label>
-          <label htmlFor="tel">
-            電話番号
-            <Input id="tel" type="tel" name="tel" placeholder="09012345678" />
-          </label>
-          <label htmlFor="email">
-            メールアドレス
+          </div>
+
+          <div>
+            <Label htmlFor="tel">電話番号*</Label>
+            <Input
+              id="tel"
+              type="tel"
+              name="tel"
+              placeholder="09012345678"
+              required
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="email">メールアドレス*</Label>
             <Input
               id="email"
               type="email"
               name="email"
               placeholder="yamadai@example.com"
+              required
             />
-          </label>
-          <label htmlFor="tel">
-            電話番号
-            <Input id="tel" type="tel" name="tel" placeholder="09012345678" />
-          </label>
+          </div>
+
+          <div>
+            <Label htmlFor="content">お問い合わせ内容*</Label>
+            <Textarea id="content" name="content" rows={8} required />
+          </div>
+
           <Button variant="secondary" type="submit">
             送信
           </Button>
