b85be7bb9939588797b1ee68aec54ebf4067bd59
osakana0630 <osakana0630@gmail.com>
2024-04-29 20:14:40 +0900

    add staff propeties

--- types/staff.ts
+++ types/staff.ts
@@ -2,6 +2,8 @@ import type { Content } from "newt-client-js";
 
 export type Staff = {
   fullName: string;
+  englishFullName: string;
+  specialityField: string;
   biography: string;
   career: string;
   profileImage: {
