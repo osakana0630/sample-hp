551f23e3881137a6b516b95b51378a0400e7bb16
osakana0630 <osakana0630@gmail.com>
2024-04-30 13:18:45 +0900

    filter articles by a tag, then no match articles

--- app/medium/tags/[tagSlug]/page.tsx
+++ app/medium/tags/[tagSlug]/page.tsx
@@ -2,6 +2,7 @@ import { MediaLayout } from "@/components/layout/media-layout";
 import { getArticlesByTagIds, getTagBySlug } from "@/lib/newt";
 import { notFound } from "next/navigation";
 import { ArticleList } from "@/components/article-list";
+import { Heading } from "@/components/heading";
 
 type Props = {
   params: { tagSlug: string };
@@ -10,11 +11,22 @@ export default async function Page({ params: { tagSlug } }: Props) {
   const tag = await getTagBySlug(tagSlug);
   if (tag === null) notFound();
   const articles = await getArticlesByTagIds([tag._id]);
+  const hasArticles = articles.length > 0;
 
   return (
-    <MediaLayout>
-      <h1>「{tag.name}」の記事一覧</h1>
-      <ArticleList articles={articles} />
+    <MediaLayout
+      pageTitle={<Heading component="h1" label="メディア" labelEn="Media" />}
+    >
+      <section>
+        <h2 className="font-bold text-lg md:text-xl mb-6">
+          「{tag.name}」の記事一覧
+        </h2>
+        {hasArticles ? (
+          <ArticleList articles={articles} />
+        ) : (
+          <p>記事が見つかりませんでした。</p>
+        )}
+      </section>
     </MediaLayout>
   );
 }
