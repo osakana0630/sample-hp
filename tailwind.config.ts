03feb379947fe83ee92808349099667bea7280dc
osakana0630 <osakana0630@gmail.com>
2024-04-30 20:04:27 +0900

    change maxWidth in @tailwindcss/typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,6 +72,13 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: "100ch", // add required value here
+          },
+        },
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
