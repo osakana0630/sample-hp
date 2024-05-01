71a36db0d4a04652238626072d1589008e0b5cc5
osakana0630 <osakana0630@gmail.com>
2024-05-01 23:20:55 +0900

    fix service list component

--- components/service-list/index.tsx
+++ components/service-list/index.tsx
@@ -1,28 +1,27 @@
-import { BUSINESSES } from "@/constants/business";
-import Image from "next/image";
+import { BUSINESSES } from '@/constants/business';
+import Image from 'next/image';
 
 export function ServiceList() {
   return (
     <ul className="space-y-6">
       {BUSINESSES.map((business) => (
-        <li
-          key={business.label}
-          className="grid grid-cols-1 md:grid-cols-3 gap-6 rounded-lg"
-        >
-          <div className="relative aspect-video w-full self-start overflow-hidden rounded-lg border">
-            <Image
-              src={business.image}
-              alt={business.label}
-              fill
-              className="object-contain object-center"
-            />
-          </div>
-          <div className="md:col-span-2">
-            <h3 className="text-xl font-semibold mb-3">{business.label}</h3>
-            <p className="text-sm text-muted-foreground leading-8 whitespace-pre-wrap">
-              {business.description}
-            </p>
-          </div>
+        <li key={business.label}>
+          <article className="grid grid-cols-1 gap-6 rounded-lg md:grid-cols-5">
+            <div className="relative aspect-video w-full self-start overflow-hidden rounded-lg border md:col-span-2">
+              <Image
+                src={business.image}
+                alt={business.label}
+                fill
+                className="object-contain object-center"
+              />
+            </div>
+            <div className="md:col-span-3">
+              <h3 className="mb-3 text-xl font-semibold">{business.label}</h3>
+              <p className="whitespace-pre-wrap text-sm leading-8 text-muted-foreground">
+                {business.description}
+              </p>
+            </div>
+          </article>
         </li>
       ))}
     </ul>
