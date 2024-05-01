c2aef3e89aec1d44c109cdc8d47528ba16483d4a
osakana0630 <osakana0630@gmail.com>
2024-05-01 20:40:29 +0900

    fix style in news detail

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,9 +1,9 @@
-import { getNewsList, getNewsById } from "@/lib/newt";
-import { BasicLayout } from "@/components/layouts/basic-layout";
-import { notFound } from "next/navigation";
-import { Heading } from "@/components/heading";
-import { CustomBreadcrumb } from "@/components/custom-breadcrumb";
-import { paths } from "@/routes";
+import { getNewsList, getNewsById } from '@/lib/newt';
+import { BasicLayout } from '@/components/layouts/basic-layout';
+import { notFound } from 'next/navigation';
+import { Heading } from '@/components/heading';
+import { CustomBreadcrumb } from '@/components/custom-breadcrumb';
+import { paths } from '@/routes';
 
 export async function generateStaticParams() {
   const { news } = await getNewsList();
@@ -14,11 +14,7 @@ export async function generateStaticParams() {
 
 export const dynamicParams = false;
 
-export default async function Page({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default async function Page({ params: { id } }: { params: { id: string } }) {
   const news = await getNewsById(id);
 
   if (!news) {
@@ -30,17 +26,14 @@ export default async function Page({
       pageTitle={<Heading component="h1" label="お知らせ" labelEn="News" />}
       breadcrumb={
         <CustomBreadcrumb
-          links={[
-            { name: "お知らせ", href: paths.newsList(1) },
-            { name: news.title },
-          ]}
+          links={[{ name: 'お知らせ', href: paths.newsList(1) }, { name: news.title }]}
         />
       }
     >
-      <div className="prose container">
-        <h1>{news.title}</h1>
+      <section className="prose">
+        <h2 className="mb-6 text-3xl font-bold">{news.title}</h2>
         <div dangerouslySetInnerHTML={{ __html: news.body }} />
-      </div>
+      </section>
     </BasicLayout>
   );
 }
